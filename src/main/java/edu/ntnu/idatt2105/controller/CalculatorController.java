4c472ade67e76284c029e5a6ef2e31c20e76ae65
Scott Sanya Emonanekkul <scottse@Scott-sin-MacBook-Air.local>
2024-02-15 15:11:13 +0100

    modified methods in CalculatorService and CalculatorController

--- src/main/java/edu/ntnu/idatt2105/controller/CalculatorController.java
+++ src/main/java/edu/ntnu/idatt2105/controller/CalculatorController.java
@@ -1,11 +1,15 @@
 package edu.ntnu.idatt2105.controller;
 
-import edu.ntnu.idatt2105.model.CalculationLog;
 import edu.ntnu.idatt2105.service.CalculatorService;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CalculatorController {
@@ -18,24 +22,10 @@ public class CalculatorController {
     }
 
     @PostMapping("/calculate")
-    public CalculationLog calculate(@RequestBody String expression) {
-        System.out.println("Received calculation request: " + expression);
-
-        CalculationLog calculationLog = calculatorService.calculate(expression);
-
-        System.out.println("Calculation result: " + calculationLog.getResult());
-
-        return calculationLog;
-    }
-
-    @PostMapping("/calculateJSON")
-    public CalculationLog calculateJSON(@RequestBody CalculationLog calculationLog) {
-        System.out.println("Received calculation request: " + calculationLog.getExpression());
-
-        CalculationLog result = calculatorService.calculate(calculationLog.getExpression());
-
-        System.out.println("Calculation result: " + result.getResult());
-
-        return result;
+    public ResponseEntity<String> calculate(@RequestBody String equation) {
+        System.out.println(equation);
+        Expression expression = new ExpressionBuilder(equation).build();
+        double result = expression.evaluate();
+        return ResponseEntity.ok(Double.toString(result));
     }
 }
--- src/main/java/edu/ntnu/idatt2105/service/CalculatorService.java
+++ src/main/java/edu/ntnu/idatt2105/service/CalculatorService.java
@@ -1,69 +1,15 @@
 package edu.ntnu.idatt2105.service;
 
-import edu.ntnu.idatt2105.model.CalculationLog;
 import org.springframework.stereotype.Service;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 @Service
 public class CalculatorService {
 
-    public CalculationLog calculate(String expression) {
-        try {
-            String[] parts = expression.split(" ");
-
-            if (parts.length != 3) {
-                throw new IllegalArgumentException("Invalid input. Expected format: operand1 operator operand2");
-            }
-
-            double operand1 = Double.parseDouble(parts[0]);
-            String operator = parts[1];
-            double operand2 = Double.parseDouble(parts[2]);
-
-            double result;
-
-            switch (operator) {
-                case "+":
-                    result = add(operand1, operand2);
-                    break;
-                case "-":
-                    result = subtract(operand1, operand2);
-                    break;
-                case "*":
-                    result = multiply(operand1, operand2);
-                    break;
-                case "/":
-                    result = divide(operand1, operand2);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid operator. Supported operators: +, -, *, /");
-            }
-
-            System.out.println("Performed calculation: " + expression + " = " + result);
-
-            return new CalculationLog(expression, result);
-
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid input. Operand must be a valid number.", e);
-        } catch (ArithmeticException e) {
-            throw new ArithmeticException("Cannot divide by zero.");
-        }
-    }
-
-    private double add(double operand1, double operand2) {
-        return operand1 + operand2;
-    }
-
-    private double subtract(double operand1, double operand2) {
-        return operand1 - operand2;
-    }
-
-    private double multiply(double operand1, double operand2) {
-        return operand1 * operand2;
-    }
-
-    private double divide(double operand1, double operand2) {
-        if (operand2 == 0) {
-            throw new ArithmeticException("Cannot divide by zero.");
-        }
-        return operand1 / operand2;
+    public double calculate (String expression) {
+        Expression expressionResult = new ExpressionBuilder(expression).build();
+        double result = expressionResult.evaluate();
+        return result;
     }
 }
